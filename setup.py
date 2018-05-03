29c930a12d36660f1a2ca3feefd823b24fd99c57
Stuart Axon <stu.axon@gmail.com>
2018-05-03 12:27:48 +0100

    Add CleanCommand to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,43 @@
-from setuptools import setup, find_packages
+from __future__ import print_function
+
+import glob
+import os
+import shutil
+import sys
+
+description = 'Integrate and control shoebot from editors or anything else',
+here = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    from setuptools import setup, Command, find_packages
+except ImportError:
+    sys.exit("Install setuptools before plugin-shoebot")
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz ./*.egg-info'.split(' ')
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        global here
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob.glob(os.path.normpath(os.path.join(here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    # Die if path in CLEAN_FILES is absolute + outside this directory
+                    raise ValueError("%s is not a path inside %s" % (path, here))
+                print('removing %s' % os.path.relpath(path))
+                shutil.rmtree(path)
 
 setup(
     name='plugin_shoebot',
@@ -6,6 +45,9 @@ setup(
     url='https://github.com/shoebot/plugin-shoebot.git',
     author='Stuart Axon',
     author_email='stu.axon@gmail.com',
-    description=' Integrate shoebot into editors and ides ',
+    description=description,
     packages=find_packages(),
+    cmdclass={
+        'clean': CleanCommand,
+    },
 )
