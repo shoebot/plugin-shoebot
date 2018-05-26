248d9227baded0f7c3e369b068797ebc7002bccf
Stuart Axon <stu.axon@gmail.com>
2018-05-26 21:36:51 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ class CleanCommand(clean):
 
 setup(
     name='plugin_shoebot',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/shoebot/plugin-shoebot.git',
     author='Stuart Axon',
     author_email='stu.axon@gmail.com',
